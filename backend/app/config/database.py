46826f497217fcf41e87c883e3222aefe32feacb
Alejo Diaz Allendez <alejo.diaz80.ad@gmail.com>
2026-01-14 15:13:16 -0300

    actualizando database.py y el .env

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,16 +1,18 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# Carga .env SOLO en local (en Railway no molesta)
 load_dotenv()
 
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = os.getenv("MYSQL_PORT")
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_USER = os.getenv("MYSQLUSER") or os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD") or os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQLHOST") or os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQLPORT") or os.getenv("MYSQL_PORT") or "3306"
+MYSQL_DB = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DB")
 
+print("MYSQL_HOST =", MYSQL_HOST)
 print("MYSQL_PORT =", MYSQL_PORT)
 
 DATABASE_URL = (
@@ -27,6 +29,7 @@ AsyncSessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
 
 async def get_db():
     async with AsyncSessionLocal() as session:

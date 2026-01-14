68cbd9c4a4e87f79921ce32f7be6d16f70ee5f75
Alejo Diaz Allendez <alejo.diaz80.ad@gmail.com>
2026-01-14 14:37:48 -0300

    actualizando back

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,12 +1,24 @@
+from dotenv import load_dotenv
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+aiomysql://root:admin@localhost:3306/clinica"
+load_dotenv()
 
-engine = create_async_engine(
-    DATABASE_URL,
-    echo=True
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
+
+print("MYSQL_PORT =", MYSQL_PORT)
+
+DATABASE_URL = (
+    f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
+    f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 )
+
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(
     bind=engine,

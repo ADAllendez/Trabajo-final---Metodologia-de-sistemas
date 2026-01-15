fc406f33065f970dcfaf9dcded6a21ed67f035da
Alejo Diaz Allendez <alejo.diaz80.ad@gmail.com>
2026-01-15 14:43:17 -0300

    comentando la linea startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,10 +22,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+#@app.on_event("startup")
+#async def startup():
+    #async with engine.begin() as conn:
+#        await conn.run_sync(Base.metadata.create_all)
 
 app.include_router(paciente.router, prefix="/pacientes", tags=["Pacientes"])
 app.include_router(especialidad.router, prefix="/especialidades", tags=["Especialidades"])

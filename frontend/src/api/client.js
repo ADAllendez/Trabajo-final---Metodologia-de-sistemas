a9f23d0c6025d2daf9a6e978c97e6bda822a14be
Alejo Diaz Allendez <alejo.diaz80.ad@gmail.com>
2026-01-15 16:45:45 -0300

    fix CORS

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  baseURL: process.env.REACT_APP_API_URL,
 });
-
-console.log("API_URL:", process.env.REACT_APP_API_URL);
 
 export default api;

947cb347e0ae056402bb0c2d0dd93c4812068dc1
Alejo Diaz Allendez <alejo.diaz80.ad@gmail.com>
2026-01-15 17:42:33 -0300

    fix main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,10 +14,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "https://localhost:5173",
+        "https://127.0.0.1:5173",
+        "https://localhost:3000",
+        "https://127.0.0.1:3000",
         "https://trabajo-final-metodologia-de-sistem.vercel.app"
 
     ],
